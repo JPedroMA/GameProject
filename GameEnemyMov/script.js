69970a37da647a99bb7f049c061e261061133ff3
João Pedro <aspuma13@gmail.com>
2024-04-21 00:31:23 -0300

    Enemy Mov
    Finalizando as movimentações de 4 modelos diferentes de inimigos, com padrões pré-definidos e velocidades aleatórias

--- GameEnemyMov/script.js
+++ GameEnemyMov/script.js
@@ -3,17 +3,158 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 4;
+const enemiesArray = [];
 
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
+let gameFrame = 0;
+
+class Enemy1 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemies/enemy1.png';
+        // this.speed = Math.random() * 4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 18 + 1);
+    }
+    update() {
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
+        // anmimate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+class Enemy2 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemies/enemy2.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 18 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 4;
+    }
+    update() {
+        this.x -= this.speed;
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = canvas.width;
+        // anmimate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+class Enemy3 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemies/enemy3.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 18 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 2;
+        this.curve = Math.random() * 200;
+    }
+    update() {
+        // PI value changes de movement pattern
+        this.x = this.curve * Math.sin(this.angle * Math.PI / 180) + canvas.width / 2 - this.width / 2;
+        this.y = this.curve * Math.cos(this.angle * Math.PI / 180) + canvas.height / 2 - this.height / 2;
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = canvas.width;
+        // anmimate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+class Enemy4 {
+    constructor() {
+        this.image = new Image();
+        this.image.src = 'enemies/enemy4.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 18 + 1);
+        this.interval = Math.floor(Math.random() * 200 + 50);
+    }
+    update() {
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/50;
+        this.y -= dy/50;
+
+        if (this.x + this.width < 0) this.x = canvas.width;
+        // anmimate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy1());
+}
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy2());
+}
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy3());
+}
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy4());
 }
 
-function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
